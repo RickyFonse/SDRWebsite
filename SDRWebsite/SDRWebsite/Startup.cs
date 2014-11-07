8bdb7233bbb1c410f7375095bf5b932806de6aa2
Rickyfonse <fonsecarichard@hotmail.com>
2014-11-06 20:21:20 -0500

    Initial Project Creation

---
+++ SDRWebsite/SDRWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDRWebsite.Startup))]
+namespace SDRWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
